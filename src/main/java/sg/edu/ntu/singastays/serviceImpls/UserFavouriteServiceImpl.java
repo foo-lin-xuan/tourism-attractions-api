fa6fd7bebced63a62edf275444e7adea58e01dc7
toonuaaa <linxuan1994@gmail.com>
2024-03-28 14:32:49 +0800

    Change in createUserFavourite to use RequestParam - to allow Category / Attraction to be optional

--- src/main/java/sg/edu/ntu/singastays/serviceImpls/UserFavouriteServiceImpl.java
+++ src/main/java/sg/edu/ntu/singastays/serviceImpls/UserFavouriteServiceImpl.java
@@ -7,15 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.persistence.EntityNotFoundException;
+import sg.edu.ntu.singastays.entities.Attraction;
+import sg.edu.ntu.singastays.entities.Category;
+import sg.edu.ntu.singastays.entities.Member;
 import sg.edu.ntu.singastays.entities.UserFavourite;
+import sg.edu.ntu.singastays.exceptions.AttractionNotFoundException;
+import sg.edu.ntu.singastays.exceptions.CategoryNotFoundException;
+import sg.edu.ntu.singastays.exceptions.MemberNotFoundException;
+import sg.edu.ntu.singastays.repositories.AttractionRepository;
+import sg.edu.ntu.singastays.repositories.CategoryRepository;
+import sg.edu.ntu.singastays.repositories.MemberRepository;
 import sg.edu.ntu.singastays.repositories.UserFavouriteRepository;
 import sg.edu.ntu.singastays.services.UserFavouriteService;
 
 @Service
 public class UserFavouriteServiceImpl implements UserFavouriteService {
-    
-    @Autowired
+
+    // @Autowired
     private UserFavouriteRepository userFavouriteRepository;
+    private MemberRepository memberRepository;
+    private CategoryRepository categoryRepository;
+    private AttractionRepository attractionRepository;
+
+    public UserFavouriteServiceImpl(UserFavouriteRepository userFavouriteRepository, MemberRepository memberRepository,
+            CategoryRepository categoryRepository, AttractionRepository attractionRepository) {
+        this.userFavouriteRepository = userFavouriteRepository;
+        this.memberRepository = memberRepository;
+        this.categoryRepository = categoryRepository;
+        this.attractionRepository = attractionRepository;
+    }
 
     @Override
     public ArrayList<UserFavourite> getAllUserFavourites() {
@@ -30,18 +50,32 @@ public class UserFavouriteServiceImpl implements UserFavouriteService {
     }
 
     @Override
-    public UserFavourite createUserFavourite(UserFavourite userFavourite) {
+    public UserFavourite createUserFavourite(Long memberId, Long categoryId, Long attractionId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(memberId));
+        Category category = null;
+        Attraction attraction = null;
+        if (categoryId != null) {
+            category = categoryRepository.findById(categoryId)
+                    .orElseThrow(() -> new CategoryNotFoundException(categoryId));
+        }
+        if (attractionId != null) {
+            attraction = attractionRepository.findById(attractionId)
+                    .orElseThrow(() -> new AttractionNotFoundException(attractionId));
+        }
+        UserFavourite userFavourite = new UserFavourite(member, category, attraction);
         return userFavouriteRepository.save(userFavourite);
     }
 
     // @Override
-    // public UserFavourite updateUserFavourite(Long id, UserFavourite userFavourite) {
-    //     UserFavourite existingUserFavourite = userFavouriteRepository.findById(id)
-    //             .orElseThrow(() -> new EntityNotFoundException("UserFavourite with id " + id + " not found"));
-    //     existingUserFavourite.setUser(userFavourite.getUser());
-    //     existingUserFavourite.setAttraction(userFavourite.getAttraction());
-    //     // Set other fields as needed
-    //     return userFavouriteRepository.save(existingUserFavourite);
+    // public UserFavourite updateUserFavourite(Long id, UserFavourite
+    // userFavourite) {
+    // UserFavourite existingUserFavourite = userFavouriteRepository.findById(id)
+    // .orElseThrow(() -> new EntityNotFoundException("UserFavourite with id " + id
+    // + " not found"));
+    // existingUserFavourite.setUser(userFavourite.getUser());
+    // existingUserFavourite.setAttraction(userFavourite.getAttraction());
+    // // Set other fields as needed
+    // return userFavouriteRepository.save(existingUserFavourite);
     // }
 
     @Override
@@ -53,9 +87,11 @@ public class UserFavouriteServiceImpl implements UserFavouriteService {
     }
 
     // @Override
-    // public UserFavourite updateUserFavourite(Long id, UserFavourite userFavourite) {
-    //     // TODO Auto-generated method stub
-    //     throw new UnsupportedOperationException("Unimplemented method 'updateUserFavourite'");
+    // public UserFavourite updateUserFavourite(Long id, UserFavourite
+    // userFavourite) {
+    // // TODO Auto-generated method stub
+    // throw new UnsupportedOperationException("Unimplemented method
+    // 'updateUserFavourite'");
     // }
 
 }
